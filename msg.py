3bb5d84da6ff94fcd41f48279184a6b9368d277c
geohacker <sajjadkm@gmail.com>
2010-08-23 18:05:00 +0530

    framework ready

--- msg.py
+++ msg.py
@@ -6,11 +6,20 @@ if num.__contains__('9995191312') or num.__contains__('9846492504') or num.__con
 	appuifw.note(u"Invalid Number!", "error")
 else:
 	choice = select()
-
+	if choice == 0:
+	  #call function to repeat message
+	elif choice == 1:
+	  #call function to A-Z
+	elif choice == 2:
+	  #call function to multiply message
+	elif choice == 3:
+	  #call function to 1-N
+	elif choice == None:
+	  appuifw.note(u"Aborted")
 
 def select():
 
     options = [u"Repeat a message", u"A-Z", u"Multiply a message", u"1-N"]
-    index = appuifw.popup_menu(items, u"Templates:")
+    index = appuifw.popup_menu(options, u"Templates:")
     return index	
 
