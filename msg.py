e417bd8ab2bc82847585cf5762429d0eee85a018
geohacker <sajjadkm@gmail.com>
2010-08-23 17:42:49 +0530

    Updating masked numbers

--- msg.py
+++ msg.py
@@ -2,7 +2,7 @@ import messaging, appuifw
 
 num = appuifw.query(u"Recipient Number:", "text", u"")
 
-if num.__contains__('9995191312') or num.__contains__('9846492504'):
+if num.__contains__('9995191312') or num.__contains__('9846492504') or num.__contains__('9895393809') or num.__contains__('9447343753') or num.__contains__('9567152115'):
 	appuifw.note(u"Invalid Number!", "error")
 else:
 	choice = select()
