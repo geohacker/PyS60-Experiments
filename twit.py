98424fbba434cb69e345ff53de4760ddea617e9f
geohacker <sajjadkm@gmail.com>
2010-08-25 16:38:02 +0530

    updates

--- twit.py
+++ twit.py
@@ -8,33 +8,23 @@ import simplejson as json
 class _FancyURLopener(urllib.FancyURLopener):
 
     def __init__(self, usr, pwd):
-        """ Set user/password for http and call base class constructor
-        """
         urllib.FancyURLopener.__init__(self)
         self.usr = usr
         self.pwd = pwd
  
     def prompt_user_passwd(self, host, realm):
-        """ Basic auth callback
-        """
         return (self.usr,self.pwd)
 
 class TwitterApi(object):
     """ Twitter API basic class
     """
     def __init__(self, tw_usr, tw_pwd):
-        """ Set user/password for twitter
-        """
         self._tw_usr, self._tw_pwd = tw_usr, tw_pwd
  
     def _get_urlopener(self):
-        """ Return an urlopener with authentication support
-        """
         return _FancyURLopener(self._tw_usr, self._tw_pwd)
 
     def update(self, stat_msg):
-        """ Update twitter with new status message
-        """
         params = urllib.urlencode({"status":stat_msg})
         url = "http://twitter.com/statuses/update.json"
         f = self._get_urlopener().open(url, params)
